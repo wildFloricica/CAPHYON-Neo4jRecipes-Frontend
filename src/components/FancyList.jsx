9c6d2a1ab3ce565c33cecaec6f4772065d156da1
wildfloricica <wildfloricica@beautifull.garden>
2024-05-01 13:58:01 +0300

    forgot to stage some changes

--- src/components/FancyList.jsx
+++ src/components/FancyList.jsx
@@ -4,16 +4,20 @@ export default function FancyList({ list = [], special = [], name = "" }) {
   special = list.filter((item) => special.includes(item));
   list = list.filter((item) => !special.includes(item));
   return (
-    <div className="flex-right fancylist">
-      <span className="fancy-title">{name}:</span>
-      {special.map((item) => (
-        <button key={crypto.randomUUID()} className="special">
-          {item}
-        </button>
-      ))}
-      {list.map((item) => (
-        <button key={crypto.randomUUID()}>{item}</button>
-      ))}
-    </div>
+    <>
+      <div className="flex-right-hugger">
+        <span className="fancy-title">{name}:</span>
+        <div className="flex-right fancylist">
+          {special.map((item) => (
+            <button key={crypto.randomUUID()} className="special">
+              {item}
+            </button>
+          ))}
+          {list.map((item) => (
+            <button key={crypto.randomUUID()}>{item}</button>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
